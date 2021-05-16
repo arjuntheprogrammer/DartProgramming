d411cf42e58d646470362d850bea1fd43cab91d9
Arjun Gupta <arjuntheprogrammer@gmail.com>
2021-05-16 17:11:01 +0530

    Exception Handling

---
+++ exception_handling.dart
@@ -0,0 +1,34 @@
+// Exception Handling
+
+int mustGreaterThanZero(int val){
+  if(val <= 0){
+    throw Exception('Value must be greater than 0');
+  }
+  return val;
+}
+
+void letVerifyTheValue(var val){
+  var valueVerification;
+
+  try{
+    valueVerification = mustGreaterThanZero(val);
+  }
+  catch(e){
+    print(e);
+  }
+  finally{
+    if(valueVerification == null){
+      print('Value is not accepted');
+    }
+    else{
+      print('Value verified: $valueVerification');
+    }
+  }
+}
+
+void main(){
+  letVerifyTheValue(10);
+  print("\n");
+
+  letVerifyTheValue(0);
+}
